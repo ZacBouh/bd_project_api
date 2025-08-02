135309da1e0afd805db748570f3d630c024f16af
ZacBouh <50382656+ZacBouh@users.noreply.github.com>
2025-08-02 17:12:21 +0000

    implemented /auth/register

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+
+final class AuthController extends AbstractController
+{
+
+    public function __construct(
+        private LoggerInterface $logger
+    ) {}
+
+    #[Route('/auth/register', name: 'auth_register', methods: 'POST')]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $jsonData = $request->getContent();
+        $this->logger->info("Received register request : $jsonData");
+        $user = $serializer->deserialize($jsonData, User::class, 'json');
+        $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hashedPassword);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'Received registration request',
+            'content' => $user,
+        ]);
+    }
+}
