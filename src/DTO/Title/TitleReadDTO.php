085b92d3194a0c1dd943ed8315a142087f48cc73
ZacBouh <50382656+ZacBouh@users.noreply.github.com>
2025-10-24 22:49:13 +0000

    added user provider to doc security firewall, added OA property description to title read dto

--- src/DTO/Title/TitleReadDTO.php
+++ src/DTO/Title/TitleReadDTO.php
@@ -5,6 +5,7 @@ namespace App\DTO\Title;
 use App\DTO\TitleContribution\ArtistTitleContributionReadDTO;
 use App\DTO\UploadedImage\UploadedImageReadDTO;
 use App\Entity\Trait\HasDefaultNormalizeCallback;
+use OpenApi\Attributes as OA;
 
 /**
  * @phpstan-import-type NormalizeCallbackDefaultReturn from HasDefaultNormalizeCallback
@@ -19,6 +20,13 @@ class TitleReadDTO
     public function __construct(
         public int $id,
         public string $name,
+        #[OA\Property(
+            type: 'object',
+            properties: [
+                new OA\Property(property: 'id', type: 'integer'),
+                new OA\Property(property: 'name', type: 'string', nullable: true),
+            ]
+        )]
         public array $publisher,
         public string $language,
         public ?string $description,
